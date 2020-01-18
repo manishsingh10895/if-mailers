6aa51d3f47542b1a451ffab5c1a2b5cbab990a2b
Manish <s_mash@Manishs-MacBook-Air.local>
2020-01-19 01:04:47 +0530

    new types' '

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,7 @@
 
 /// <reference types="node" />
 
-export default class Stringer {
+export  class Stringer {
     static getEmailConfirmation(firstName: string, link: string): string;
     static getForgotPassword(firstName: string, link: string): string;
     static getDeposit(firstName:string, symbol:string, fullName:string, amount:string): string;
